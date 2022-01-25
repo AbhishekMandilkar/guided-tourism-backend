e015fd6c3911e6a704c0e1984f35488c567e77e4
AbhishekMandilkar <66326016+AbhishekMandilkar@users.noreply.github.com>
2022-01-25 20:45:06 +0530

    added new route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 var distance = require("google-distance-matrix");
 distance.key(process.env.GOOGLE_API_KEY);
 app.use(express.json());
@@ -8,7 +7,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
-
+app.get("/getMetrics", (req, res) => {
+  res.send("Hi");
+});
 app.post("/getMetrics", (req, res) => {
   let { org, dst } = req.body;
   var origins = [`${org.latitude},${org.longitude}`];
@@ -22,6 +23,11 @@ app.post("/getMetrics", (req, res) => {
       // console.log(distance, time);
     }
   });
+});
+
+app.post("/getHotels", (req, res) => {
+  console.log(req.body);
+  res.send("API Hit");
 });
 
 app.listen(3000, () => {
