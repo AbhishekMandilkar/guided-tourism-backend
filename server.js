8822c6c3a947f225701d00c0f27772e3a39862b1
Abhishek <a.a.mandilkar@gmail.com>
2022-04-10 13:20:08 +0530

    payments integration v1

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const cors = require("cors");
 var distance = require("google-distance-matrix");
 const fs = require("firebase-admin");
 require("dotenv").config();
@@ -11,13 +14,15 @@ fs.initializeApp({
 const db = fs.firestore();
 distance.key(process.env.GOOGLE_API_KEY);
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
+
 app.get("/get-metrics", (req, res) => {
   res.send("Hi");
 });
+
 app.post("/get-metrics", (req, res) => {
   let { org, dst } = req.body;
   var origins = [`${org?.latitude},${org?.longitude}`];
@@ -51,6 +56,60 @@ app.post("/get-hotels", async (req, res) => {
     }
   });
   res.json(hotelsData);
+});
+app.use("/stripe", express.raw({ type: "*/*" }));
+app.post("/stripe", async (req, res) => {
+  // Get the signature from the headers
+  const sig = req.headers["stripe-signature"];
+  let event;
+  try {
+    // Check if the event is sent from Stripe or a third party
+    // And parse the event
+    event = await stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    // Handle what happens if the event is not from Stripe
+    console.log(err);
+    return res.status(400).json({ message: err.message });
+  }
+  // Event when a payment is initiated
+  if (event.type === "payment_intent.created") {
+    console.log(`${event.data.object.metadata.name} initated payment!`);
+  }
+  // Event when a payment is succeeded
+  if (event.type === "payment_intent.succeeded") {
+    console.log(`${event.data.object.metadata.name} succeeded payment!`);
+    // fulfilment
+  }
+  res.json({ ok: true });
+});
+app.post("/payment", async (req, res) => {
+  try {
+    // Getting data from client
+    let { amount, name } = req.body;
+    // Simple validation
+    if (!amount || !name)
+      return res.status(400).json({ message: "All fields are required" });
+    amount = parseInt(amount);
+    // Initiate payment
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "INR",
+      payment_method_types: ["card"],
+      metadata: { name },
+    });
+    // Extracting the client secret
+    const clientSecret = paymentIntent.client_secret;
+    // Sending the client secret as response
+    res.json({ message: "Payment initiated", clientSecret });
+  } catch (err) {
+    // Catch any error and send error 500 to client
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
