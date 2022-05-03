8d1d5bd650eeb005bc0d473b2616075bcdf14ccf
Abhishek <a.a.mandilkar@gmail.com>
2022-05-03 19:05:08 +0530

    fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
@@ -14,7 +15,6 @@ fs.initializeApp({
 const db = fs.firestore();
 distance.key(process.env.GOOGLE_API_KEY);
 app.use(express.json());
-app.use(require("body-parser").raw({ type: "*/*" }));
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from server");
@@ -59,31 +59,35 @@ app.post("/get-hotels", async (req, res) => {
   res.json(hotelsData);
 });
 
-app.post("/payment", async (req, res) => {
-  try {
-    // Getting data from client
-    let { amount, name, authUserId, date } = req.body;
-    // Simple validation
-    if (!amount || !name)
-      return res.status(400).json({ message: "All fields are required" });
-    // amount = parseInt(amount);
-    // Initiate payment
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "INR",
-      payment_method_types: ["card"],
-      metadata: { name, authUserId, date },
-    });
-    // Extracting the client secret
-    const clientSecret = paymentIntent.client_secret;
-    // Sending the client secret as response
-    res.json({ message: "Payment initiated", clientSecret });
-  } catch (err) {
-    // Catch any error and send error 500 to client
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
+app.post(
+  "/payment",
+
+  async (req, res) => {
+    try {
+      // Getting data from client
+      let { amount, name, authUserId, date } = req.body;
+      // Simple validation
+      if (!amount || !name)
+        return res.status(400).json({ message: "All fields are required" });
+      // amount = parseInt(amount);
+      // Initiate payment
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "INR",
+        payment_method_types: ["card"],
+        metadata: { name, authUserId, date },
+      });
+      // Extracting the client secret
+      const clientSecret = paymentIntent.client_secret;
+      // Sending the client secret as response
+      res.json({ message: "Payment initiated", clientSecret });
+    } catch (err) {
+      // Catch any error and send error 500 to client
+      console.error(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   }
-});
+);
 
 const server = app.listen(process.env.PORT || 3000, () => {
   const port = server.address().port;
@@ -91,31 +95,35 @@ const server = app.listen(process.env.PORT || 3000, () => {
 });
 
 //WEB HOOKS
-app.post("/stripe", async (req, res) => {
-  // Get the signature from the headers
-  const sig = req.headers["stripe-signature"];
-  let event;
-  try {
-    // Check if the event is sent from Stripe or a third party
-    // And parse the event
-    event = await stripe.webhooks.constructEvent(
-      req.body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    // Handle what happens if the event is not from Stripe
-    console.log(err);
-    return res.status(400).json({ message: err.message });
+app.post(
+  "/stripe",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    // Get the signature from the headers
+    const sig = req.headers["stripe-signature"];
+    let event;
+    try {
+      // Check if the event is sent from Stripe or a third party
+      // And parse the event
+      event = await stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      // Handle what happens if the event is not from Stripe
+      console.log(err);
+      return res.status(400).json({ message: err.message });
+    }
+    // Event when a payment is initiated
+    if (event.type === "payment_intent.created") {
+      console.log(`${event.data.object.metadata.name} initated payment!`);
+    }
+    // Event when a payment is succeeded
+    if (event.type === "payment_intent.succeeded") {
+      console.log(`${event.data.object.metadata.name} succeeded payment!`);
+      // fulfilment
+    }
+    res.json({ ok: true });
   }
-  // Event when a payment is initiated
-  if (event.type === "payment_intent.created") {
-    console.log(`${event.data.object.metadata.name} initated payment!`);
-  }
-  // Event when a payment is succeeded
-  if (event.type === "payment_intent.succeeded") {
-    console.log(`${event.data.object.metadata.name} succeeded payment!`);
-    // fulfilment
-  }
-  res.json({ ok: true });
-});
+);
