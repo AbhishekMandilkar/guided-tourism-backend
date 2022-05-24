7f8970505d729bd08bf5a134e8bfcfb495d561a1
Abhishek <a.a.mandilkar@gmail.com>
2022-05-24 19:32:14 +0530

    fix4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
