817e1955ca2f9704a91d5fd4a571d7a2cbd5aa9f
Abhishek <a.a.mandilkar@gmail.com>
2022-05-24 19:30:12 +0530

    fix

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ fs.initializeApp({
 });
 const db = fs.firestore();
 distance.key(process.env.GOOGLE_API_KEY);
-app.use((req, res, next) => {
-  if (req.originalUrl === "/stripe") {
-    next();
-  } else {
-    bodyParser.json()(req, res, next);
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.originalUrl === "/stripe") {
+//     next();
+//   } else {
+//     bodyParser.json()(req, res, next);
+//   }
+// });
+app.use(require("body-parser").raw({ type: "*/*" }));
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from server");
